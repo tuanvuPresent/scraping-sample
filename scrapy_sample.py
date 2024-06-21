3bd72874d24fed13f8a1eb1d86208cc6b1b6f488
vunt378 <55792941+tuanvuPresent@users.noreply.github.com>
2024-06-21 22:50:52 +0700

    Update scrapy_sample.py

--- scrapy_sample.py
+++ scrapy_sample.py
@@ -1,9 +1,43 @@
+import os
 from datetime import datetime
 from typing import Any
 
 import scrapy
 from scrapy.crawler import CrawlerProcess
+from scrapy.exporters import CsvItemExporter
 from scrapy.http import Response
+
+
+class ItemPipeline(object):
+    files = {}
+    exporters = {}
+    csv_item_types = []
+
+    def open_spider(self, spider):
+        feeds = spider.custom_settings.get('MULTI_CSV_FEEDS', {})
+        self.csv_item_types = feeds.keys()
+        for output_path, feed_config in feeds.items():
+            file_path = os.path.join(spider.custom_settings.get('FILES_STORE', ''), output_path)
+            file_exists = os.path.exists(file_path)
+            self.files[output_path] = open(file_path, 'ab')
+            if file_exists:
+                include_headers_line = True
+            else:
+                include_headers_line = False
+            self.exporters[output_path] = CsvItemExporter(self.files[output_path],
+                                                          include_headers_line=include_headers_line)
+
+        [e.start_exporting() for e in self.exporters.values()]
+
+    def close_spider(self, spider):
+        [e.finish_exporting() for e in self.exporters.values()]
+        [f.close() for f in self.files.values()]
+
+    def process_item(self, item, spider):
+        output_path_name = item.pop('output_path_name', None)
+        if output_path_name in set(self.csv_item_types):
+            self.exporters[output_path_name].export_item(item)
+        return item
 
 
 class SampleSpider(scrapy.Spider):
