08cfd4b3becb7aefd2e8e62bd85b4c88564efa36
vunt378 <55792941+tuanvuPresent@users.noreply.github.com>
2024-06-21 22:48:57 +0700

    Update selenium_sample.py

--- selenium_sample.py
+++ selenium_sample.py
@@ -1,3 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
 class SeleniumRequest:
     def __init__(self):
         service = Service(executable_path=ChromeDriverManager().install())
